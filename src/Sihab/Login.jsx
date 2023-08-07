9e3087888b3d8a508eadf4d3a0025373b8cc62e7
unknown <sihabuddinmolla11@gmail.com>
2023-08-07 18:43:33 +0600

    first commit

---
+++ src/Sihab/Login.jsx
@@ -0,0 +1,13 @@
+const Login = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
