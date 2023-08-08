506828bc01635b558f578de7e92310bb730e5726
Atikur <atikurrahman7223@gmail.com>
2023-08-08 12:34:11 +0600

    testing

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,6 +2,8 @@ const Home = () => {
     return (
       <div>
         <h2 className="text-2xl font-bold text-teal-500">Atikur Rahman</h2>
+        <h2>development brach</h2>
+        <h2>development brach testing</h2>
       </div>
     );
 };
