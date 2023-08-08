31515b7d2f32b0cd2d3ae0c6c1207d653fb6d0ae
unknown <sihabuddinmolla11@gmail.com>
2023-08-08 12:38:16 +0600

    check commit

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
         <h2 className="text-2xl font-bold text-teal-500">Atikur Rahman</h2>
         <h2>development brach</h2>
         <h2>development brach testing</h2>
+        <p>Sihab Molla</p>
       </div>
     );
 };
