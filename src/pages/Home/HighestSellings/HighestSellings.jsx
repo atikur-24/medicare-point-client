da9da4f09a4e2d1c93f376b8f73c60b7087cb50b
NafeesIS <nafeesshawon555@gmail.com>
2023-08-08 17:22:28 +0600

    test

---
+++ src/pages/Home/HighestSellings/HighestSellings.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HighestSellings = () => {
+  return (
+    <div>
+      <h1>Highest Selling Medicines</h1>
+    </div>
+  );
+};
+
+export default HighestSellings;
