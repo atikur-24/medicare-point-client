4f8738ece2056d48d0dfc8e48ab4f7283de0e1ff
safayatalif <safayatalif2021@gmail.com>
2023-08-09 18:35:42 +0600

    create a home page banner section

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Banner from "../Banner/Banner";
 import Categories from "../Categories/Categories";
 import HighestSellings from "../HighestSellings/HighestSellings";
 
 const Home = () => {
   return (
     <div>
+      <Banner />
       <Categories />
       <HighestSellings />
     </div>
