c86fe6cf6b566d2b20da4b88c76cab22c762b3a8
Abdullah149081 <masudmd425@gmail.com>
2023-08-10 00:19:01 +0600

    ad banner section for pc

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,15 @@
+import Ad from "../Ad/Ad";
+import DeliveryAd from "../Ad/DeliveryAd";
 import Categories from "../Categories/Categories";
 import HighestSellings from "../HighestSellings/HighestSellings";
 
 const Home = () => {
   return (
     <div>
+      <Ad />
       <Categories />
       <HighestSellings />
+      <DeliveryAd />
     </div>
   );
 };
