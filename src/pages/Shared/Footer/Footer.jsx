316bc49c4cea251ac15ff2a9f363eda6006d3f02
unknown <sihabuddinmolla11@gmail.com>
2023-08-09 09:32:29 +0600

    create navbar done

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,10 +1,9 @@
-
 const Footer = () => {
-    return (
-      <div>
-        Footer
-      </div>
-    );
+  return (
+    <div>
+      <h1>Footer</h1>
+    </div>
+  );
 };
 
 export default Footer;
