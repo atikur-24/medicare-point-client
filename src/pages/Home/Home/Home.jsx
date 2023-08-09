2bbcdb5e24996180cbed88f47ec42191538d4f4a
NafeesIS <nafeesshawon555@gmail.com>
2023-08-09 21:25:21 +0600

    two section added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,15 @@
 import Categories from "../Categories/Categories";
+import Feedback from "../FeedBack/Feedback";
 import HighestSellings from "../HighestSellings/HighestSellings";
+import OtcMedicine from "../OtcMedicine/OtcMedicine";
 
 const Home = () => {
   return (
     <div>
       <Categories />
       <HighestSellings />
+      <OtcMedicine />
+      <Feedback />
     </div>
   );
 };
