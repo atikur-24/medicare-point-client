4eed59cad7f56e6bb3550c58a226817bc6e422e1
unknown <sihabuddinmolla11@gmail.com>
2023-08-10 11:43:03 +0600

    product section done

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import Banner from "../Banner/Banner";
 import Categories from "../Categories/Categories";
 import HighestSellings from "../HighestSellings/HighestSellings";
 import chatImg from "../../../assets/icon/message_icon.svg";
+import Product1 from "../Product1/Product1";
 
 const Home = () => {
   return (
@@ -9,12 +10,13 @@ const Home = () => {
       <Banner />
       <Categories />
       <HighestSellings />
+      <Product1 />
 
       <div className="fixed bottom-10 right-10 ">
         <img
           className="w-12 md:w-20 hover:w-24 cursor-pointer ease-in-out duration-500"
           src={chatImg}
-          alt=""
+          alt="Message Icon"
         />
       </div>
     </div>
