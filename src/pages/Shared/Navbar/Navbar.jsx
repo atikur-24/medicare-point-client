4be9751fdde9afbd2ffc063d1aaa84a15c3abbac
unknown <sihabuddinmolla11@gmail.com>
2023-08-11 01:49:46 +0600

    apull

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,12 +1,17 @@
-import { NavLink } from "react-router-dom";
+/* eslint-disable react/jsx-wrap-multilines */
+import { Menu, MenuButton, MenuItem } from "@szhsin/react-menu";
+import "@szhsin/react-menu/dist/index.css";
+import "@szhsin/react-menu/dist/transitions/slide.css";
+import { Link, NavLink } from "react-router-dom";
 import Avater from "./Avater/Avater";
 import Lang from "./Language/Lang";
 import Logo from "./Logo/Logo";
 import NavCard from "./NavCard/NavCard";
+import ResponsiveNavbar from "./ResponsivNavbar/ResponsiveNavbar";
 import Search from "./Search/Search";
 
 const Navbar = () => {
-  const MenuItems = (
+  const menuItems = (
     <>
       <li className="hover:text-neutral-900 ">
         <NavLink to="#">Home</NavLink>
@@ -43,32 +48,50 @@ const Navbar = () => {
   return (
     <div className="bg-[#d9d9d9]">
       <div className="nav-container ">
-        <div className="flex items-center justify-between py-2">
-          <div className="flex items-center gap-10 justify-between">
-            <Logo />
-            <Search />
-          </div>
-          <div className="flex items-center gap-4">
-            <Lang />
-            <NavCard />
-            <hr className=" border-r-2 h-12 border-[#6b6a6a] " />
-            <div>
-              <p className="font-semibold">
-                Login/ <br />
-                Register
-              </p>
+        <div className=" hidden md:block">
+          <div className="flex items-center justify-between py-2 ">
+            <div className="flex items-center gap-10 justify-between">
+              <Logo />
+              <Search />
             </div>
-            <Avater />
+            <div className="flex items-center gap-4">
+              <Lang />
+              <NavCard />
+              <div>
+                <Menu
+                  menuButton={
+                    <MenuButton>
+                      <Avater />
+                    </MenuButton>
+                  }
+                  transition
+                >
+                  <MenuItem>
+                    <Link className=" font-semibold text-neutral-600" to="/login">
+                      Login
+                    </Link>
+                  </MenuItem>
+                  <MenuItem>
+                    <Link className=" font-semibold text-neutral-600" to="/register">
+                      Register
+                    </Link>
+                  </MenuItem>
+                </Menu>
+              </div>
+            </div>
           </div>
         </div>
       </div>
       <hr className=" w-full shadow-md   border-[#999999]" />
-      <div className="bg-[#d9d9d9] nav-container">
-        <div className="py-2">
-          <ul className="flex items-center justify-between text-md text-neutral-600 font-semibold">
-            {MenuItems}
-          </ul>
+      <div className="hidden md:block">
+        <div className="bg-[#d9d9d9] nav-container ">
+          <div className="py-2">
+            <ul className="flex items-center justify-between text-md text-neutral-600 font-semibold">{menuItems}</ul>
+          </div>
         </div>
+      </div>
+      <div className=" md:hidden">
+        <ResponsiveNavbar menuItems={menuItems} />
       </div>
     </div>
   );
