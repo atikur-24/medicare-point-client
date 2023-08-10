19e5c0f2a49f590726dca42b7c932a56a8726d20
unknown <sihabuddinmolla11@gmail.com>
2023-08-10 10:42:31 +0600

    Fixed footer

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,7 +1,7 @@
 import Banner from "../Banner/Banner";
 import Categories from "../Categories/Categories";
 import HighestSellings from "../HighestSellings/HighestSellings";
-import chatImg from "../../../assets/icon/facebook-messenger.svg";
+import chatImg from "../../../assets/icon/message_icon.svg";
 
 const Home = () => {
   return (
@@ -10,8 +10,12 @@ const Home = () => {
       <Categories />
       <HighestSellings />
 
-      <div className="fixed bottom-10 right-8 ">
-        <img className="w-12 md:w-16 cursor-pointer" src={chatImg} alt="" />
+      <div className="fixed bottom-10 right-10 ">
+        <img
+          className="w-12 md:w-20 hover:w-24 cursor-pointer ease-in-out duration-500"
+          src={chatImg}
+          alt=""
+        />
       </div>
     </div>
   );
