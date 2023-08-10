fb9cafb22a4e096bf62a4c734248c37a6895c7d5
NafeesIS <nafeesshawon555@gmail.com>
2023-08-10 16:25:40 +0600

    feedback srction edited

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -5,12 +5,9 @@ import Categories from "../Categories/Categories";
 import ChooseUs from "../ChooseUs/ChooseUs";
 import Feedback from "../FeedBack/Feedback";
 import HighestSellings from "../HighestSellings/HighestSellings";
-<<<<<<< HEAD
 import chatImg from "../../../assets/icon/message_icon.svg";
 import Product1 from "../Product1/Product1";
-=======
 import OtcMedicine from "../OtcMedicine/OtcMedicine";
->>>>>>> a6d627951365017ed3d745cc9e006d5e0e1c7b8c
 
 const Home = () => {
   return (
@@ -19,22 +16,14 @@ const Home = () => {
       <Ad />
       <Categories />
       <HighestSellings />
-<<<<<<< HEAD
       <Product1 />
-
-      <div className="fixed bottom-10 right-10 ">
-        <img
-          className="w-12 md:w-20 hover:w-24 cursor-pointer ease-in-out duration-500"
-          src={chatImg}
-          alt="Message Icon"
-        />
-      </div>
-=======
       <OtcMedicine />
       <Feedback />
       <DeliveryAd />
       <ChooseUs />
->>>>>>> a6d627951365017ed3d745cc9e006d5e0e1c7b8c
+      <div className="fixed bottom-10 right-10 ">
+        <img className="w-12 md:w-20 hover:w-24 cursor-pointer ease-in-out duration-500" src={chatImg} alt="Message Icon" />
+      </div>
     </div>
   );
 };
