0b4f96c9e371891bcdb82fb9181d8f8d6cb90e40
Abdullah149081 <masudmd425@gmail.com>
2023-08-10 14:07:36 +0600

    home page chat img z-50 fixed

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
       <HighestSellings />
       <Product1 />
 
-      <div className="fixed bottom-10 right-10 ">
+      <div className="fixed z-50 bottom-10 right-10 ">
         <img className="w-12 md:w-20 hover:w-24 cursor-pointer ease-in-out duration-500" src={chatImg} alt="Message Icon" />
       </div>
       <OtcMedicine />
