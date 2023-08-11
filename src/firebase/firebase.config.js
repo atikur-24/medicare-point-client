c2adddbbf005eddfe5cc4d125c73d0291b423cf7
Atikur <atikurrahman7223@gmail.com>
2023-08-11 11:35:06 +0600

    firebae auth setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBdxK3shgq0bZNCncrKJH47yEk5XmqJ_8U",
+  authDomain: "medicare-point.firebaseapp.com",
+  projectId: "medicare-point",
+  storageBucket: "medicare-point.appspot.com",
+  messagingSenderId: "972043707868",
+  appId: "1:972043707868:web:96acae092d8bac68491b19",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
