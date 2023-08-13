e21c93e63032e49607ca37943badb5377fa04412
Abdullah149081 <masudmd425@gmail.com>
2023-08-13 23:15:44 +0600

    new Advertisement

---
+++ src/pages/Home/Advertisement/DeliveryOffer.jsx
@@ -0,0 +1,12 @@
+import delivery from "../../../assets/images/AD/delivery.png";
+import OfferBanner from "./OfferBanner";
+
+const DeliveryOffer = () => {
+  return (
+    <div className=" hidden md:block">
+      <OfferBanner discount="5" img_ad={delivery} title="Free Delivery" />
+    </div>
+  );
+};
+
+export default DeliveryOffer;
