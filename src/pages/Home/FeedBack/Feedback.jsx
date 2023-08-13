c6a22b465fa7b727c6a93363485db36e93598e4e
Abdullah149081 <masudmd425@gmail.com>
2023-08-14 00:12:16 +0600

    all fix

--- src/pages/Home/FeedBack/Feedback.jsx
+++ src/pages/Home/FeedBack/Feedback.jsx
@@ -1,16 +1,16 @@
 /* eslint-disable import/no-unresolved */
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Rating from "react-rating";
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { FreeMode, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
-import Rating from "react-rating";
 
-import "./styles.css";
 import SectionTitle from "../../../components/SectionTitle";
+import "./styles.css";
 
 const Feedback = () => {
   const [feedbacks, setFeedbacks] = useState([]);
@@ -21,7 +21,10 @@ const Feedback = () => {
 
   return (
     <div className="my-container">
-      <SectionTitle title="Customer Feedback" content="Our customers have spoken, highlighting the authenticity of our products, the exceptional level of service they receive, and the reliability of our health solutions. Your feedback fuels our drive to continuously improve and serve you better." />
+      <SectionTitle
+        title="Customer Feedback"
+        content="Our customers have spoken, highlighting the authenticity of our products, the exceptional level of service they receive, and the reliability of our health solutions. Your feedback fuels our drive to continuously improve and serve you better."
+      />
       <Swiper
         slidesPerView={1}
         spaceBetween={10}
@@ -48,17 +51,19 @@ const Feedback = () => {
       >
         {feedbacks.map((feedback, idx) => (
           <SwiperSlide key={idx}>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-4 h-full mb-10 px-4">
-              <div className="bgc-salt-3 h-full p-6 rounded-lg shadow-xl ">
-                <div className="grid justify-center items-center mb-4">
-                  <h3 className="text-lg font-semibold -mb-20">{feedback.name}</h3>
-                  <div className="grid items-center justify-center translate-y-20">
-                    {/* Adjust the width and height of the image */}
-                    <img src={feedback.image} alt={feedback.name} style={{ height: "80px", width: "80px" }} className="rounded-full w-16" />
-                    <Rating readonly initialRating={feedback.rating} emptySymbol={<span className="text-gray-300">&#9734;</span>} fullSymbol={<span className="text-yellow-400">&#9733;</span>} />
+            <div className="">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-4  mb-10 px-4">
+                <div className="bg-card p-6 rounded-lg shadow-xl ">
+                  <div className="grid justify-center items-center mb-4">
+                    <h3 className="text-lg font-semibold -mb-20">{feedback.name}</h3>
+                    <div className="grid items-center justify-center translate-y-20">
+                      {/* Adjust the width and height of the image */}
+                      <img src={feedback.image} alt={feedback.name} style={{ height: "80px", width: "80px" }} className="rounded-full w-16" />
+                      <Rating readonly initialRating={feedback.rating} emptySymbol={<span className="text-gray-300">&#9734;</span>} fullSymbol={<span className="text-yellow-400">&#9733;</span>} />
+                    </div>
                   </div>
+                  <p className="text-gray-200 h-44 p-4 pt-20 rounded bgc-salt-6 text-justify">{feedback.quote.slice(0, 100)}</p>
                 </div>
-                <p className="text-gray-200 p-4 pt-20 rounded bgc-salt-6 text-justify">{feedback.quote}</p>
               </div>
             </div>
           </SwiperSlide>
