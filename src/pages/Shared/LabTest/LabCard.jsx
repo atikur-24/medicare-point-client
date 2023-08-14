788619414c4908fc6cdc23c891f5a4db4b597b5f
Abdullah149081 <masudmd425@gmail.com>
2023-08-14 23:40:53 +0600

    lab card design is done

--- src/pages/Shared/LabTest/LabCard.jsx
+++ src/pages/Shared/LabTest/LabCard.jsx
@@ -6,17 +6,17 @@ const LabCard = ({ category }) => {
       </figure>
       <div className="">
         <h2 className="text-xl font-sans font-bold h-16">{category?.category_name}</h2>
-        <p className="text-sm text-[#10847e] font-bold font-nunito">20 tests includs</p>
+        <p className="text-sm text-my-primary font-bold font-nunito">20 tests includs</p>
       </div>
-      <hr className="border border-[#10847e] my-2" />
+      <hr className="border border-my-primary my-2" />
       <div className="flex gap-2 items-center font-bold">
         <span>BDT </span>
         <span className="line-through">৳300</span>
-        <span className="badge badge-accent text-white py-0.5">20% off</span>
+        <span className="badge bg-my-primary border-0 badge-accent text-white py-0.5">20% off</span>
       </div>
-      <p className="font-bold my-2">৳200</p>
+      <p className="font-bold my-2 text-my-pink">৳200</p>
       <div className="">
-        <button type="button" className="text-[#10847e] font-extrabold uppercase font-nunito ">
+        <button type="button" className="text-my-primary font-extrabold uppercase font-nunito ">
           Add To Cart
         </button>
       </div>
