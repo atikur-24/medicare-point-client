4122b12e78983d38d9f5f1761016deef9e21b96e
unknown <sihabuddinmolla11@gmail.com>
2023-08-15 11:46:46 +0600

    fixed firebase error

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
+/* eslint-disable comma-dangle */
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import AuthProvider from "./contexts/AuthProvider";
 import "./index.css";
 import router from "./routes/Routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <AuthProvider>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </AuthProvider>
 );
