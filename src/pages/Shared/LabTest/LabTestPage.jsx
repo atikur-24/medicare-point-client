39ae651fcd017a7d20f5cab3a7efe59f08e5e15e
safayatalif <safayatalif2021@gmail.com>
2023-08-15 20:48:57 +0600

    create a lab test page

--- src/pages/Shared/LabTest/LabTestPage.jsx
+++ src/pages/Shared/LabTest/LabTestPage.jsx
@@ -9,7 +9,7 @@ const LabTestPage = () => {
   }, []);
   return (
     <div className="my-container">
-      <div className="grid grid-cols-6">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         {labItems.map((cat, idx) => (
           <LabCard key={idx} labItem={cat} />
         ))}
