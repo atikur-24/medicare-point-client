6bb5fd5cc6ab1aff17d8819192dcd4aeefa9d599
Abdullah149081 <masudmd425@gmail.com>
2023-08-16 13:51:02 +0600

    labTEst page data render is done

--- src/pages/Shared/LabTest/LabTestPage.jsx
+++ src/pages/Shared/LabTest/LabTestPage.jsx
@@ -1,17 +1,26 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import LabCard from "./LabCard";
 
 const LabTestPage = () => {
+  const [labCategory, setLabCategory] = useState([]);
   const [labItems, setLabItems] = useState([]);
+  const { id } = useParams();
+
   useEffect(() => {
-    axios.get("/public/labTestItems.json").then((res) => setLabItems(res?.data));
-  }, []);
+    axios.get(`http://localhost:5000/labCategory/${id}`).then((res) => setLabCategory(res?.data));
+  }, [id]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:5000/labItems/${labCategory[0]?.category_name}`).then((res) => setLabItems(res?.data));
+  }, [labCategory, labCategory.category_name]);
+
   return (
     <div className="my-container">
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-        {labItems.map((cat, idx) => (
-          <LabCard key={idx} category={cat} />
+        {labItems.map((items) => (
+          <LabCard key={items._id} category={items} />
         ))}
       </div>
     </div>
--- src/pages/Shared/LabTest/PopularCategories.jsx
+++ src/pages/Shared/LabTest/PopularCategories.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const PopularCategories = ({ category }) => {
   return (
-    <Link to={`/labPage/${category.id}`} className="bg-white border border-gray-3 py-4 rounded-lg flex justify-between items-center flex-col h-[200px]">
+    <Link to={`/labPage/${category._id}`} className="bg-white border border-gray-3 py-4 rounded-lg flex justify-between items-center flex-col h-[200px]">
       <figure className="bg-[#f1f5f9] p-2 rounded-full">
         <img src={category?.image_url} alt="Category_photo" className=" rounded-md w-20 h-20" />
       </figure>
