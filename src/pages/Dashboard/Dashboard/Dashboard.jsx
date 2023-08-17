b1e2f44282d91d7449af7fc3262cc66153aec82f
tapfer-abdullah <tapfer.abdullah@gmail.com>
2023-08-17 23:55:06 +0600

    Made some temporary changes

--- src/pages/Dashboard/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard/Dashboard.jsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react";
+import useAuth from "../../../hooks/useAuth";
 import AdminHome from "../AdminDashboard/AdminHome/AdminHome";
-// import PharmacistsHome from "../PharmacistDashboard/PharmacistsHome/PharmacistsHome";
+
+import PharmacistsHome from "../PharmacistDashboard/PharmacistsHome/PharmacistsHome";
 
 const Dashboard = () => {
+  const { role } = useAuth();
+  const [isUser, setUser] = useState(false);
+  const [isPharmacist, setPharmacist] = useState(false);
+  const [isAdmin, setAdmin] = useState(false);
+
+  useEffect(() => {
+    if (role === "User") {
+      setUser(true);
+    } else if (role === "Pharmacist") {
+      setPharmacist(true);
+    } else if (role === "Admin") {
+      setAdmin(true);
+    }
+  }, []);
   return (
     <div>
-      <AdminHome />
-      {/* <PharmacistsHome /> */}
+      {isAdmin && <AdminHome />}
+      {isPharmacist && <PharmacistsHome />}
     </div>
   );
 };
