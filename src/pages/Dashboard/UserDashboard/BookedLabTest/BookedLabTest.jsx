2f0a55923716857294325229d071e8bdd0689107
NafeesIS <nafeesshwaon555@gmail.com>
2023-08-17 11:25:13 +0600

    user dashboard done

--- src/pages/Dashboard/UserDashboard/BookedLabTest/BookedLabTest.jsx
+++ src/pages/Dashboard/UserDashboard/BookedLabTest/BookedLabTest.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+
 const BookedLabTest = () => {
   const [bookedlabtestitems, setbookedlabtestitems] = useState([]);
 
@@ -9,7 +10,42 @@ const BookedLabTest = () => {
       .then((res) => setbookedlabtestitems(res.data))
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
-  return <div></div>;
+
+  return (
+    <div className="p-6">
+      <table className="w-full border-collapse">
+        <thead>
+          <tr>
+            <th className="py-2 px-4 text-left">Test Name</th>
+            <th className="py-2 px-4 text-left">Price (BDT)</th>
+            <th className="py-2 px-4 text-left">Lab Test Date</th>
+            <th className="py-2 px-4 text-left">Lab Test Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          {bookedlabtestitems.map((bookedlabtestitem, index) => (
+            <tr key={index} className="border-b">
+              <td className="py-4 px-4">
+                <div className="flex items-center space-x-2">
+                  <img src={bookedlabtestitem.image_url} alt="" className="h-10 w-10 rounded-full" />
+                  <span className="text-sm font-semibold">{bookedlabtestitem.test_name}</span>
+                </div>
+              </td>
+              <td className="py-4 px-4">
+                <span className="font-bold">৳{bookedlabtestitem.price}</span>
+              </td>
+              <td className="py-4 px-4">
+                <p className="font-semibold">{bookedlabtestitem.date}</p>
+              </td>
+              <td className="py-4 px-4">
+                <p className="font-semibold">{bookedlabtestitem.time}</p>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default BookedLabTest;
--- src/pages/HealthTips/HealthTips.jsx
+++ src/pages/HealthTips/HealthTips.jsx
@@ -13,7 +13,7 @@ const HealthTips = () => {
       <h1 className="text-4xl text-center font-bold my-4">Health Tips</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 m-8 ">
         {healthTips.map((healthTip, index) => (
-          <div key={index} className="grid grid-cols-3 gap-4 w-full h-full justify-center items-center shadow-xl bgc-white border p-3">
+          <div key={index} className="grid grid-cols-3 gap-4 w-full h-full justify-center items-center rounded-md shadow-sm border border-gray-3 p-3">
             <img src={healthTip.image} alt="img" className="h-32" />
             <h2 className="inline-flex items-center gap-3 font-medium text-[16px] tracking-wide">{healthTip.name}</h2>
             <Link to={`/healthtips/${healthTip.id}`} className="flex justify-end">
