c4814cf0e309f38b7c6697fa71620fe59e9c9f5e
unknown <sihabuddinmolla11@gmail.com>
2023-08-18 19:52:15 +0600

    fixed some isue

--- src/pages/Shared/Navbar/NavCard/NavCart.jsx
+++ src/pages/Shared/Navbar/NavCard/NavCart.jsx
@@ -3,8 +3,8 @@ import { FaCartPlus } from "react-icons/fa";
 const NavCart = () => {
   return (
     <div className="relative py-3">
-      <FaCartPlus className="h-6 w-6 lg:h-8 lg:w-8 txt-primary" />
-      <p className="absolute bottom-5 left-6 lg:left-8 lg:bottom-7 text-xl font-medium">0</p>
+      <FaCartPlus className="h-6 w-6 lg:h-8 lg:w-8 text-my-primary" />
+      <p className="absolute bottom-5 left-6 lg:left-8 lg:bottom-7 text-white py-[1px] px-2 rounded-full bg-my-accent font-medium">0</p>
     </div>
   );
 };
