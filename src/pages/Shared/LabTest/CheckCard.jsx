f5d45dc4ccd7d363b2cf61791c38eb9c78842bfa
Abdullah149081 <masudmd425@gmail.com>
2023-08-20 16:47:57 +0600

    logo add

--- src/pages/Shared/LabTest/CheckCard.jsx
+++ src/pages/Shared/LabTest/CheckCard.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 const CheckCard = () => {
   return (
-    <div className="container mx-auto">
-      <div className="bg-white  w-full md:w-96  rounded-l-lg border-4  border-[#f59e0b] z-40 fixed  p-6 bottom-0 ">
+    <div className="container mx-auto relative">
+      <div className="bg-white  w-full md:w-96  rounded-l-lg border-4  border-[#f59e0b] z-40 fixed  p-6 bottom-0 right-0 ">
         <div className="space-y-2">
           <p className="text-lg text-my-primary font-medium">2 Items added to your cart</p>
           <Link to="/labPayment" className="my-btn">
