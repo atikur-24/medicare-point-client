783cfab5ca99b7ff26c85209fb04e15391330e43
Abdullah149081 <masudmd425@gmail.com>
2023-08-20 20:03:22 +0600

    navbar active link add

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,8 +23,8 @@ export default {
       "gray-4": "#7d879c",
       "gray-5": "#64748b",
       "gray-6": "#4b5563",
-      "my-accent": "#16b4ac",
-      "my-primary": "#10847e",
+      "my-accent": "#10B6A8",
+      "my-primary": "#006F70",
       "slate-1": "#f1f5f9",
       "slate-3": "#cbd5e1",
       "slate-6": "#475569",
@@ -39,3 +39,4 @@ export default {
   },
   plugins: [require("daisyui")],
 };
+
