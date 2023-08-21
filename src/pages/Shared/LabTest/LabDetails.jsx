e28b583e4290e4ebc88fd54b9679eb998d9bb435
Abdullah149081 <masudmd425@gmail.com>
2023-08-21 17:01:42 +0600

    lab test check card design is done

--- src/pages/Shared/LabTest/LabDetails.jsx
+++ src/pages/Shared/LabTest/LabDetails.jsx
@@ -1,10 +1,12 @@
 import { FaAngleDown } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import LabAddCard from "./LabAddCard";
 
 const LabDetails = ({ open, toggleOpen }) => {
   return (
-    <div className={`${open ? "hidden" : "block"} z-10 fixed  bg-lite border-[3px]  border-[#FCB716] rounded-l-lg h-[670px] md:w-96 w-full overflow-auto right-0 bottom-0  `}>
+    <div className={`${open ? "hidden" : "block"} z-10 fixed  bg-[#F2FBFF] border-[3px]  border-[#FCB716] rounded-l-lg h-[670px] md:w-96 w-full overflow-auto right-0 bottom-0  `}>
       <div className="relative">
-        <div className="fixed rounded-t-lg left-1 right-4 md:left-auto md:right-auto p-4 bg-card  drop-shadow-lg">
+        <div className="fixed rounded-t-lg left-1 right-4 md:left-auto md:right-auto p-4 bg-card  drop-shadow-lg z-10">
           <div className="flex gap-6 justify-evenly">
             <p className="text-lg text-title-color font-medium">1 Item added to your cart</p>
             <button onClick={toggleOpen} type="button" className=" capitalize text-my-primary font-medium inline-flex items-center">
@@ -16,50 +18,20 @@ const LabDetails = ({ open, toggleOpen }) => {
           </div>
         </div>
       </div>
-      <div className="p-4 my-10">
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt non quod modi omnis voluptatibus cupiditate minima ab quo delectus nulla earum libero asperiores, quia labore similique
-          obcaecati dolore sunt aperiam assumenda. Animi officiis similique temporibus quaerat dolore porro minus aliquid vel unde obcaecati saepe odio ut asperiores corrupti tempora distinctio at,
-          sed perferendis magni veritatis culpa vero, eos voluptatem iste. Odio doloremque quo consequuntur eos voluptatem nisi consectetur aliquam sit quas cupiditate earum aperiam amet perspiciatis,
-          error labore magnam officiis facere ullam deleniti, quisquam doloribus accusamus iusto sed voluptates! Perferendis ut est magni saepe ab mollitia pariatur in omnis non quod quisquam natus
-          quam unde corporis blanditiis doloremque, nesciunt maxime deserunt officiis illum possimus aut alias quae minus. Delectus, voluptas aperiam ab quos fugit perferendis dolores laborum nemo
-          ullam suscipit consectetur asperiores animi libero eaque vel inventore consequuntur dolor rerum unde pariatur repellendus distinctio esse alias natus. Inventore esse voluptatibus iste
-          tempore doloribus, veritatis ullam dicta amet quia, saepe aperiam corrupti incidunt et sint doloremque nam obcaecati, expedita quo iusto odio optio ut? Voluptatum iure dignissimos molestias
-          consequuntur similique! Esse consectetur aut quidem tempore quibusdam quasi nihil fuga maxime omnis hic fugiat deleniti natus quo, fugit quam asperiores nam facere distinctio vitae
-          dignissimos harum corrupti ex quis deserunt. Cum dolorem voluptas repellendus nostrum ab nemo similique, optio commodi quis debitis minus. Hic ad eligendi omnis, nobis maxime similique id
-          maiores quas neque dicta, fugit dolores aliquid amet eveniet non iusto adipisci eos iure eius. Eligendi doloremque, autem quos totam minus quis a tenetur quod pariatur temporibus deleniti?
-          Ipsam ipsa praesentium blanditiis? Id eos ut expedita exercitationem nisi eligendi quos corporis nam ratione, nihil deserunt sit omnis earum placeat officia consectetur commodi delectus,
-          repellat quae odit natus harum distinctio numquam reprehenderit? Autem repudiandae reiciendis non rem esse officiis explicabo quo quasi magni, enim voluptatum rerum magnam ab, laudantium
-          quae blanditiis, itaque possimus iusto vitae tempora repellendus. Voluptate facilis fuga, veniam similique quo in sint eaque veritatis adipisci commodi ex asperiores odit sapiente,
-          reprehenderit dolores! Cum optio porro eveniet sunt mollitia architecto accusantium tempore quaerat qui, labore consequatur necessitatibus illo sed quia praesentium eos fugiat natus iste
-          eius laborum soluta voluptas ut id. Vitae a quaerat quisquam tempore. Atque voluptate, natus nam ducimus consequuntur dolorum ipsum aliquid nesciunt voluptates ipsa obcaecati? Dignissimos,
-          non placeat ipsa sint obcaecati esse iure iusto temporibus! Ipsam, quod dignissimos. Ipsa facilis, reiciendis enim nihil tenetur distinctio asperiores. Aut odit aliquid eum mollitia
-          officiis. Illum eum at voluptates in dolore ducimus ex? Quaerat facere praesentium assumenda neque voluptate tenetur dolorum illo, aliquam vero optio nihil, maiores omnis qui quibusdam
-          excepturi officiis, dolore obcaecati laborum modi! Laboriosam libero, fugit aperiam, incidunt deleniti dolores corporis perferendis magnam sit veritatis optio! Mollitia quam nam error
-          quaerat, tempora alias, nihil totam possimus aspernatur recusandae saepe consequatur voluptatem corrupti, blanditiis dolorum expedita. Ad dignissimos exercitationem voluptatum quibusdam a
-          velit suscipit, commodi mollitia culpa eius quis laboriosam pariatur magnam neque! Consequatur minus tempore perspiciatis possimus ea ducimus reiciendis nostrum, fugiat eligendi ullam
-          provident, vitae dolorum sed. Officia, ut dolorem? Commodi, quae. Velit perspiciatis voluptas laboriosam sed veritatis consequuntur ut quaerat molestias reiciendis modi aspernatur ducimus
-          illum et aperiam dolorum ipsam, debitis magni labore impedit obcaecati repellat architecto assumenda perferendis. Aut esse voluptatibus quasi eos accusantium non labore, eius corrupti
-          praesentium sit! Impedit incidunt, aliquam quam, consequatur dignissimos laboriosam quos ipsum nam, consectetur eaque corrupti qui asperiores! Quod, facere neque! Molestias, dolore possimus
-          est, minima, perferendis odio nobis nihil architecto alias cum sequi. Id, harum illum, voluptas eum ipsam quod laborum, quia totam est tempore magnam? Optio, repudiandae similique
-          perferendis officiis, aspernatur facere, fugiat in officia quod itaque a accusamus voluptatem illo sint illum delectus culpa consequuntur nihil? Quasi nemo provident suscipit labore atque
-          repudiandae amet at omnis, saepe repellat nihil similique cum minima perspiciatis veritatis sapiente doloremque praesentium accusantium consequuntur commodi architecto reiciendis eius error?
-          Voluptate libero saepe tempora dolorem aspernatur fuga, iste culpa vitae earum quas. Debitis corporis nulla unde beatae tempore animi odit nemo vero, hic quos porro? Deleniti corrupti, magni
-          in natus iure libero illo repudiandae atque. Id esse assumenda reiciendis maxime hic dolor molestias repellendus at nobis velit. Consequuntur ea itaque maiores quis dolorum ipsam! Ratione
-          veritatis error officiis recusandae facere. Optio atque aperiam modi. Quibusdam modi eaque consequatur voluptatibus atque odit quasi, ullam facere laborum, labore fuga, aliquid error fugit
-          eius in beatae non voluptates! Inventore minima consectetur officia culpa illum voluptatem corrupti quos eos esse corporis excepturi, animi quidem non? Asperiores, exercitationem omnis. Eum,
-          quibusdam quod esse architecto velit eos? Debitis, maxime dolorum. In id, cupiditate quidem cum corrupti explicabo amet sed! Suscipit qui, nulla fugit voluptates adipisci sapiente, autem
-          vitae ad dolores accusamus in sequi corrupti architecto quas. Blanditiis, consequuntur illum voluptate magni corporis quis voluptates doloremque eligendi ad quos dolore nihil beatae? Cum
-          veniam atque aliquid illo sapiente ut, tempora repellendus velit nemo quae ducimus natus fugit. Natus quos, aut consequatur nobis alias perferendis nemo, vitae, facilis quisquam corporis
-          quam velit exercitationem? Ipsum totam repellat unde eum commodi aliquid facilis sequi optio. Eius laudantium consequatur voluptatem quidem dignissimos error minus quod, in ab? Nulla nostrum
-          dolorum quisquam, omnis qui, esse, sit aut unde dicta sunt impedit similique! Porro repellat dicta distinctio sequi repudiandae itaque adipisci rerum minus, quasi delectus saepe. Quas,
-          dignissimos cupiditate quia a perspiciatis facilis beatae repellendus amet nobis inventore obcaecati enim magnam eius sapiente qui ipsum perferendis? Autem, quam magni inventore, eum itaque
-          distinctio adipisci neque voluptatum minus corporis placeat, omnis quisquam! Accusantium necessitatibus veritatis pariatur facere, fugit quis hic saepe, voluptatem ab amet architecto rerum
-          reprehenderit sequi perspiciatis molestiae ea mollitia recusandae! Blanditiis, incidunt adipisci debitis esse quam consectetur, itaque beatae sunt sapiente doloremque ut ab nobis, in ad
-          maiores. Consequatur obcaecati quae facere mollitia odio nemo officia blanditiis, dolorum tenetur, nulla commodi, aut recusandae excepturi. Labore porro nesciunt tenetur pariatur. Nihil
-          adipisci esse laboriosam. Sit et consectetur cum eaque illo fugit, praesentium hic odit saepe, aliquam repellendus nemo quam sunt dolores nostrum perspiciatis tenetur voluptate id. Ut odit
-          quis corporis officia.
-        </p>
+      <div className="p-4 mt-10 mb-20">
+        <LabAddCard />
+        <LabAddCard />
+        <LabAddCard />
+        <LabAddCard />
+        <LabAddCard />
+
+        <div className=" w-full md:w-96 fixed  bottom-0  right-0 p-4 bg-card   z-10 border-x-[3px]  border- border-[#FCB716]">
+          <Link to="/labPayment">
+            <button type="button" className="my-btn w-full">
+              Proceed
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
