514919c86f7946b373a647810bc3cfcab6c3d2b1
tapfer-abdullah <tapfer.abdullah@gmail.com>
2023-08-21 20:30:50 +0600

    Update labtest done

--- src/pages/Dashboard/AdminDashboard/AllAvailableTest/AddLabTest.jsx
+++ src/pages/Dashboard/AdminDashboard/AllAvailableTest/AddLabTest.jsx
@@ -65,7 +65,9 @@ const AddLabTest = () => {
 
     const formData = new FormData();
     formData.append("image", image);
-    const url = `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_KEY}`;
+    const url = `https://api.imgbb.com/1/upload?key=${
+      import.meta.env.VITE_IMGBB_KEY
+    }`;
     fetch(url, {
       method: "POST",
       body: formData,
@@ -93,37 +95,72 @@ const AddLabTest = () => {
         <div>
           <img className="w-4/5 mx-auto" src={addTestImg} alt="" />
         </div>
-        <form onSubmit={handleSubmit(onSubmit)} className="admission-form doctor-form">
-          <h3 className="text-center text-xl md:text-3xl font-semibold my-5 text-my-primary">Add A New Lab Test</h3>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="admission-form doctor-form"
+        >
+          <h3 className="text-center text-xl md:text-3xl font-semibold my-5 text-my-primary">
+            Add A New Lab Test
+          </h3>
           <div className="divider" />
 
           <div className="two-input-field lg:flex gap-5">
             <div>
               <label className="label">
-                <span className="label-text md:text-base font-bold font-nunito">Test Name</span>
+                <span className="label-text md:text-base font-bold font-nunito">
+                  Test Name
+                </span>
               </label>
-              <input required placeholder="Enter lab test name" type="text" {...register("test_name")} className="w-full max-w-md  outline-none" />
+              <input
+                required
+                placeholder="Enter lab test name"
+                type="text"
+                {...register("test_name")}
+                className="w-full max-w-md  outline-none"
+              />
             </div>
             <div>
               <label className="label">
-                <span className="label-text md:text-base font-bold font-nunito">Price</span>
+                <span className="label-text md:text-base font-bold font-nunito">
+                  Price
+                </span>
               </label>
-              <input required placeholder="Enter lab test name" type="number" {...register("price")} className="w-full max-w-md outline-none" />
+              <input
+                required
+                placeholder="Enter lab test name"
+                type="number"
+                {...register("price")}
+                className="w-full max-w-md outline-none"
+              />
             </div>
             <div>
               <label className="label">
-                <span className="label-text md:text-base font-bold font-nunito">Discount</span>
+                <span className="label-text md:text-base font-bold font-nunito">
+                  Discount
+                </span>
               </label>
-              <input required placeholder="Enter fees" defaultValue={0} type="number" {...register("discount")} className="w-full max-w-md outline-none" />
+              <input
+                required
+                placeholder="Enter fees"
+                defaultValue={0}
+                type="number"
+                {...register("discount")}
+                className="w-full max-w-md outline-none"
+              />
             </div>
           </div>
 
           <div className="two-input-field lg:flex gap-5">
             <div>
               <label className="label">
-                <span className="label-text md:text-base font-bold font-nunito">Category Name</span>
+                <span className="label-text md:text-base font-bold font-nunito">
+                  Category Name
+                </span>
               </label>
-              <select {...register("category_name", { required: true })} className="select select-bordered w-full max-w-md outline-none">
+              <select
+                {...register("category_name", { required: true })}
+                className="select select-bordered w-full max-w-md outline-none"
+              >
                 {labCategories.map((option) => (
                   <option key={option} value={option}>
                     {option}
@@ -133,18 +170,50 @@ const AddLabTest = () => {
             </div>
             <div>
               <label className="label">
-                <span className="label-text md:text-base font-bold font-nunito">Phone Number</span>
+                <span className="label-text md:text-base font-bold font-nunito">
+                  Phone Number
+                </span>
               </label>
-              <input required type="text" placeholder="Enter lab's phone Number" {...register("PhoneNumber")} className="w-full max-w-md outline-none" />
+              <input
+                required
+                type="text"
+                placeholder="Enter lab's phone Number"
+                {...register("PhoneNumber")}
+                className="w-full max-w-md outline-none"
+              />
+            </div>
+          </div>
+
+          <div className="two-input-field lg:flex gap-5">
+            <div>
+              <h4>City</h4>
+              <input
+                type="text"
+                placeholder="Enter city"
+                {...register("city", { required: true })}
+              />
+            </div>
+            <div>
+              <h4>All labs (separated by &)</h4>
+              <input
+                type="text"
+                placeholder="Enter lab names"
+                {...register("labNames")}
+              />
             </div>
           </div>
 
           <div className="two-input-field lg:flex gap-5">
             <div className="">
               <label className="label">
-                <span className="label-text md:text-base font-bold font-nunito">Category</span>
+                <span className="label-text md:text-base font-bold font-nunito">
+                  Category
+                </span>
               </label>
-              <select {...register("category", { required: true })} className="select select-bordered w-full max-w-md">
+              <select
+                {...register("category", { required: true })}
+                className="select select-bordered w-full max-w-md"
+              >
                 {labCategory.map((option) => (
                   <option key={option} value={option}>
                     {option}
@@ -154,18 +223,34 @@ const AddLabTest = () => {
             </div>
             <div className="input-btn">
               <label className="label">
-                <span className="label-text md:text-base font-bold font-nunito">Photo</span>
+                <span className="label-text md:text-base font-bold font-nunito">
+                  Photo
+                </span>
               </label>
 
-              <input required type="file" {...register("image_url")} className="w-full  max-w-md " />
+              <input
+                required
+                type="file"
+                {...register("image_url")}
+                className="w-full  max-w-md "
+              />
             </div>
           </div>
 
           <div>
             <label className="label">
-              <span className="label-text md:text-base font-bold font-nunito">Details About The Test</span>
+              <span className="label-text md:text-base font-bold font-nunito">
+                Details About The Test
+              </span>
             </label>
-            <textarea name="labTest-details" placeholder="Write details about the test" cols="30" rows="5" className="labtest-details" {...register("labTest-details")} />
+            <textarea
+              name="labTest-details"
+              placeholder="Write details about the test"
+              cols="30"
+              rows="5"
+              className="labtest-details"
+              {...register("labTest-details")}
+            />
           </div>
 
           <div>
