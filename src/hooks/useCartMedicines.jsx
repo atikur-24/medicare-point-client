84687021db78905f8b36276b1118deaea331269d
unknown <sihabuddinmolla11@gmail.com>
2023-08-23 15:34:41 +0600

    start calculation

--- src/hooks/useCartMedicines.jsx
+++ src/hooks/useCartMedicines.jsx
@@ -2,15 +2,15 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 const useCartMedicines = () => {
-    const { data: cart = [], refetch } = useQuery({
-        queryKey: ["carts"],
-        queryFn: async () => {
-            const data = await axios.get("http://localhost:5000/medicineCarts");
-            return data;
-        },
-    });
+  const { data: cart = [], refetch } = useQuery({
+    queryKey: ["carts"],
+    queryFn: async () => {
+      const data = await axios.get("http://localhost:5000/medicineCarts");
+      return data.data;
+    },
+  });
 
-    return [cart, refetch];
+  return [cart, refetch];
 };
 
 export default useCartMedicines;
