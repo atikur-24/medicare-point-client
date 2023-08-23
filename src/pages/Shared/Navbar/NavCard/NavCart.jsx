2e91629f0abe4761134a50282aa9bbe36ca7a296
unknown <sihabuddinmolla11@gmail.com>
2023-08-23 15:34:41 +0600

    start calculation

--- src/pages/Shared/Navbar/NavCard/NavCart.jsx
+++ src/pages/Shared/Navbar/NavCard/NavCart.jsx
@@ -8,7 +8,7 @@ const NavCart = () => {
   return (
     <Link to="/medicineCarts" className="relative py-3">
       <FaCartPlus className="h-6 w-6 lg:h-8 lg:w-8 text-my-primary" />
-      <p className="absolute bottom-5 left-6 lg:left-8 lg:bottom-7 text-white py-[1px] px-2 rounded-full bg-my-accent font-medium">{cart?.data?.length}</p>
+      <p className="absolute bottom-5 left-6 lg:left-8 lg:bottom-7 text-white py-[1px] px-2 rounded-full bg-my-accent font-medium">{cart?.length}</p>
     </Link>
   );
 };
