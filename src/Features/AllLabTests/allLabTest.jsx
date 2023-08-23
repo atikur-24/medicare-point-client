e4e4a2f01c33b1168c9428db65b14c0b92461c9c
tapfer-abdullah <tapfer.abdullah@gmail.com>
2023-08-23 13:18:33 +0600

    redux added in Lab test's all components

---
+++ src/Features/AllLabTests/allLabTest.jsx
@@ -0,0 +1,33 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchAllLabTests = createAsyncThunk("allLabTests/fetchAllLabTests", async () => {
+  const res = await axios.get("http://localhost:5000/labAllItems");
+  return res.data;
+});
+
+const allLabTestSlice = createSlice({
+  name: "allLabTests",
+  initialState: {
+    isLoading: false,
+    error: null,
+    allLabTest: [],
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchAllLabTests.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchAllLabTests.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.allLabTest = action.payload;
+      state.error = null;
+    });
+    builder.addCase(fetchAllLabTests.rejected, (state, action) => {
+      state.isLoading = false;
+      state.allLabTest = [];
+      state.error = action.error.message;
+    });
+  },
+});
+
+export default allLabTestSlice.reducer;
