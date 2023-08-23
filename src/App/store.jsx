3abe93d2ff0416bc8f76f5e2ce13cf30401e2d00
tapfer-abdullah <tapfer.abdullah@gmail.com>
2023-08-23 13:18:33 +0600

    redux added in Lab test's all components

--- src/App/store.jsx
+++ src/App/store.jsx
@@ -1,7 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
+import addLabTest from "../Features/AllLabTests/addLabTest";
+import allLabTest from "../Features/AllLabTests/allLabTest";
+import deleteLabTest from "../Features/AllLabTests/deleteLabTest";
+import updateLabTest from "../Features/AllLabTests/updateLabTest";
+import allMedicines from "../Features/AllMedicines/allMedicines";
+import allUsers from "../Features/AllUsers/allUsers";
+import uploadImage from "../Features/UploadImage/uploadImage";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    allUsers,
+    allMedicines,
+    allLabTest,
+    updateLabTest,
+    uploadImage,
+    deleteLabTest,
+    addLabTest,
+  },
 });
 
 export default store;
