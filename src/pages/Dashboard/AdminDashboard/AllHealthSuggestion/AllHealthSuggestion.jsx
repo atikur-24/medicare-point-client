6982ad98be9e215045a9f86644d6220d8b4ad8fb
NafeesIS <nafeesshwaon555@gmail.com>
2023-08-23 21:01:20 +0600

    add tips added

--- src/pages/Dashboard/AdminDashboard/AllHealthSuggestion/AllHealthSuggestion.jsx
+++ src/pages/Dashboard/AdminDashboard/AllHealthSuggestion/AllHealthSuggestion.jsx
@@ -1,82 +1,73 @@
-import { BsBoxArrowInUpRight } from "react-icons/bs";
-import { FaPencilAlt } from "react-icons/fa";
-import { RiDeleteBinLine } from "react-icons/ri";
+import { useForm } from "react-hook-form";
 
 const AllHealthSuggestion = () => {
-  return (
-    <div className="px-5">
-      <h3 className="text-center text-xl md:text-3xl my-7 font-semibold">
-        All Helth Suggestion and Tips
-      </h3>
+  const { register, handleSubmit } = useForm();
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
-        <div className="md:flex shadow-lg bg-card">
-          <img
-            className="md:h-full md:w-[220px]"
-            src="https://www.planstreetinc.com/wp-content/uploads/2021/07/what-is-mental-health-1130x675.png"
-            alt=""
-          />
-          <div className="p-4 space-y-2">
-            <h3 className="text-xl font-semibold">Healthy Diet</h3>
-            <p className="text-justify">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa
-              facere eveniet nihil pariatur.....
-            </p>
-            <div className="space-x-2 text-center">
-              <button
-                type="button"
-                className="btn btn-circle btn-sm bg-my-primary text-white hover:bg-my-accent"
-              >
-                <FaPencilAlt className="text-lg" />
-              </button>
-              <button
-                type="button"
-                className="btn btn-circle btn-sm bg-red-500 text-white hover:bg-my-pink"
-              >
-                <RiDeleteBinLine className="text-lg" />
-              </button>
-              <button
-                type="button"
-                className="btn btn-circle btn-sm hover:bg-my-primary text-white bg-my-accent"
-              >
-                <BsBoxArrowInUpRight className="text-lg" />
-              </button>
-            </div>
-          </div>
+  const onSubmit = (data) => {
+    // Image Upload
+    const image = data.image[0];
+    // console.log(image);
+    const formData = new FormData();
+    formData.append("image", image);
+    const url = `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_KEY}`;
+    fetch(url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((imageData) => {
+        data.image_url = imageData.data.display_url;
+        console.log(data);
+      });
+    console.log("New Health Tip:", data);
+  };
+
+  return (
+    <div className="text-center">
+      <div className="grid grid-cols-2">
+        <div>
+          <img className="w-full" src="https://st2.depositphotos.com/1561359/6865/v/450/depositphotos_68650923-stock-illustration-doctor-writing-healthy-tips-words.jpg" alt="" />
         </div>
-        <div className="md:flex shadow-lg bg-card">
-          <img
-            className="md:h-full md:w-[220px]"
-            src="https://www.srbija.gov.rs/static/en/65781/0/img_65781.jpg"
-            alt=""
-          />
-          <div className="p-4 space-y-2">
-            <h3 className="text-xl font-semibold">Title here</h3>
-            <p className="text-justify">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa
-              facere eveniet nihil pariatur......
-            </p>
-            <div className="space-x-2 text-center">
-              <button
-                type="button"
-                className="btn btn-circle btn-sm bg-my-primary text-white hover:bg-my-accent"
-              >
-                <FaPencilAlt className="text-lg" />
-              </button>
-              <button
-                type="button"
-                className="btn btn-circle btn-sm bg-red-500 text-white hover:bg-my-pink"
-              >
-                <RiDeleteBinLine className="text-lg" />
-              </button>
-              <button
-                type="button"
-                className="btn btn-circle btn-sm hover:bg-my-primary text-white bg-my-accent"
-              >
-                <BsBoxArrowInUpRight className="text-lg" />
-              </button>
+        <div className="bg-gray-3 p-2">
+          <h1 className="text-2xl font-bold py-2">Add A New Health Tips</h1>
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+            <div className="grid grid-cols-2 gap-2">
+              <div>
+                <label className="text-sm font-semibold">Category</label>
+                <input type="text" {...register("category")} className="input input-bordered w-full" />
+              </div>
+              <div>
+                <label className="text-sm font-semibold">Name</label>
+                <input type="text" {...register("name")} className="input input-bordered w-full" />
+              </div>
             </div>
-          </div>
+            <div>
+              <label className="text-sm font-semibold">Image</label>
+              <input type="file" {...register("image", { required: true })} className="file-input file-input-bordered file-input-accent w-full" />
+            </div>
+            {/* Repeat for other fields */}
+            <div className="grid grid-cols-2 gap-2">
+              <div>
+                <label className="text-sm font-semibold">Type</label>
+                <input type="text" {...register("type")} className="input input-bordered w-full" />
+              </div>
+              <div>
+                <label className="text-sm font-semibold">Cause</label>
+                <input type="text" {...register("cause")} className="input input-bordered w-full" />
+              </div>
+            </div>
+            <div>
+              <label className="text-sm font-semibold">Prevention</label>
+              <textarea {...register("prevention")} className="textarea textarea-bordered w-full" />
+            </div>
+            <div>
+              <label className="text-sm font-semibold">Cure</label>
+              <textarea {...register("cure")} className="textarea textarea-bordered w-full" />
+            </div>
+            <button type="submit" className="my-btn">
+              Add Health Tip
+            </button>
+          </form>
         </div>
       </div>
     </div>
