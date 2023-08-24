55cd0620715513ccf27acc78864198544ee06870
Atikur <atikurrahman7223@gmail.com>
2023-08-24 15:58:12 +0600

    fixed refetch

--- src/components/AddCartButton.jsx
+++ src/components/AddCartButton.jsx
@@ -1,17 +1,24 @@
 import axios from "axios";
 import { BiCartAdd } from "react-icons/bi";
-import { toast } from "react-toastify";
 import Swal from "sweetalert2";
 import { useLocation, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import useAuth from "../hooks/useAuth";
+import useCartMedicines from "../hooks/useCartMedicines";
 
 const AddCartButton = ({ cartMedicine, cls }) => {
+  const [, refetch] = useCartMedicines();
   const { user } = useAuth();
   const navigate = useNavigate();
   const location = useLocation();
   const handleAddToCart = () => {
     if (user) {
-      axios.post("http://localhost:5000/medicineCarts", cartMedicine).then(() => toast.success("Item Added Success", { position: "top-center", theme: "colored", autoClose: 3000, pauseOnHover: false }));
+      axios.post("http://localhost:5000/medicineCarts", cartMedicine).then((result) => {
+        if (result.data.insertedId || result.data.modifiedCount) {
+          toast.success("Item Added Success", { position: "top-center", theme: "colored", autoClose: 3000, pauseOnHover: false });
+          refetch();
+        }
+      });
     } else {
       Swal.fire({
         title: "Please login for cart item",
