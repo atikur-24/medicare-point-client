272195935f3b90b29a661eaceb478c67ca622314
Abdullah149081 <masudmd425@gmail.com>
2023-08-24 13:26:35 +0600

    lab add & remove is done

--- src/pages/Shared/LabTest/LabButton.jsx
+++ src/pages/Shared/LabTest/LabButton.jsx
@@ -1,6 +1,37 @@
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useLocation, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import useAuth from "../../../hooks/useAuth";
+import useLabCart from "../../../hooks/useLabCart";
+
 const LabButton = ({ labAddCart }) => {
+  const [, refetch] = useLabCart();
+  const { user } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
   const handlerLab = () => {
-    console.log(labAddCart);
+    if (user) {
+      axios.post("http://localhost:5000/labsCart", labAddCart).then((res) => {
+        if (res.data.insertedId) {
+          refetch();
+          toast.success("Lab Added Success", { position: "top-center", theme: "colored", autoClose: 3000, pauseOnHover: false });
+        }
+      });
+    } else {
+      Swal.fire({
+        title: "Please login for cart item",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#16b4ac",
+        cancelButtonColor: "#ef4444",
+        confirmButtonText: "Login Now",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate("/login", { state: { from: location } });
+        }
+      });
+    }
   };
   return (
     <div className="">
@@ -12,3 +43,10 @@ const LabButton = ({ labAddCart }) => {
 };
 
 export default LabButton;
+
+// axios.post("http://localhost:5000/pharmacyRegistrationApplication", application).then((res) => {
+//   // console.log(res.data);
+//   if (res.data.insertedId) {
+//     Swal.fire("Successful", "Your Application Success fully added", "success");
+//   }
+// });
