cca20036412fb6542355b7ec83cf3beee2fe746e
tapfer-abdullah <tapfer.abdullah@gmail.com>
2023-08-27 22:29:39 +0600

    add new blog option added

--- src/pages/Medicines/MedicineReviews.jsx
+++ src/pages/Medicines/MedicineReviews.jsx
@@ -1,14 +1,13 @@
 import { Rating, StickerStar } from "@smastrom/react-rating";
 import { MdLocationOn } from "react-icons/md";
 
+const customStyles = {
+  itemShapes: StickerStar,
+  activeFillColor: "#fbb614",
+  inactiveFillColor: "#DEE1E6",
+};
 const MedicineReviews = ({ allRatings }) => {
   // console.log(allRatings);
-
-  const customStyles = {
-    itemShapes: StickerStar,
-    activeFillColor: "#fbb614",
-    inactiveFillColor: "#DEE1E6",
-  };
 
   return (
     <div className="pl-3">
@@ -16,7 +15,9 @@ const MedicineReviews = ({ allRatings }) => {
         {allRatings.map((singleR) => (
           <div key={singleR?._id}>
             <div className="flex justify-between items-center mb-5">
-              <Rating style={{ maxWidth: 100 }} value={singleR?.rating} readOnly itemStyles={customStyles} />
+              <div>
+                <Rating style={{ maxWidth: 100 }} value={singleR?.rating} readOnly itemStyles={customStyles} />
+              </div>
               <p>{singleR.date}</p>
             </div>
             <p className="text-gray-5 text-justify">{singleR?.reviewMessage}</p>
