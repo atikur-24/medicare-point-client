1249b8180cd5445663b57143e4f90df47818d339
tapfer-abdullah <tapfer.abdullah@gmail.com>
2023-08-29 11:35:44 +0600

    success page desing

--- src/App/store.jsx
+++ src/App/store.jsx
@@ -6,10 +6,16 @@ import updateLabTest from "../Features/AllLabTests/updateLabTest";
 import allMedicines from "../Features/AllMedicines/allMedicines";
 import allUsers from "../Features/AllUsers/allUsers";
 import addBlog from "../Features/Blogs/addBlog";
+import deleteBlog from "../Features/Blogs/deleteBlog";
+import updateBlog from "../Features/Blogs/updateBlog";
 import addHealthTips from "../Features/HealthTips/addHealthTips";
 import allHealthTips from "../Features/HealthTips/allHealthTips";
-import { deleteHealthTipsApi } from "../Features/HealthTips/deleteHealthTips";
+import deleteHealthTips from "../Features/HealthTips/deleteHealthTips";
 import updateHealthTips from "../Features/HealthTips/updateHealthTips";
+import addInterview from "../Features/Interviews/addInterview";
+import deleteInterview from "../Features/Interviews/deleteInterview";
+import updateInterview from "../Features/Interviews/updateInterview";
+import sslPayment from "../Features/PaymentGetway/PaymentGetaway";
 import uploadImage from "../Features/UploadImage/uploadImage";
 
 const store = configureStore({
@@ -23,9 +29,15 @@ const store = configureStore({
     addLabTest,
     addHealthTips,
     allHealthTips,
-    deleteHealthTipsApi,
+    deleteHealthTips,
     updateHealthTips,
     addBlog,
+    updateBlog,
+    deleteBlog,
+    addInterview,
+    updateInterview,
+    deleteInterview,
+    sslPayment,
   },
 });
 
