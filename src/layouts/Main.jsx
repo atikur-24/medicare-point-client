ece006e3f636c86b494e6f3f947a43ab138a24d6
tapfer-abdullah <tapfer.abdullah@gmail.com>
2023-08-31 12:42:49 +0600

    change popup location

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,12 +1,21 @@
+import { useEffect, useState } from "react";
+import { Toaster } from "react-hot-toast";
 import { Outlet, ScrollRestoration } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import { Toaster } from "react-hot-toast";
+import PopUp from "../pages/Home/PopUp/PopUp";
 import Footer from "../pages/Shared/Footer/Footer";
 import Navbar from "../pages/Shared/Navbar/Navbar";
 
 const Main = () => {
+  const [isPopUp, setPopUp] = useState("-top-[1000px]");
+  useEffect(() => {
+    setTimeout(() => {
+      setPopUp("top-[100px]");
+    }, 3000);
+  }, []);
   return (
     <>
+      <PopUp isPopUp={isPopUp} setPopUp={setPopUp} />
       <Navbar />
       <div className="min-h-[calc(100vh-100px)] bg-white">
         <Outlet />
