b5da0a2e7b86cf14858f3aadabfbe1c4ea3cb1a8
Atikur <atikurrahman7223@gmail.com>
2023-09-01 19:08:45 +0600

    show all medicine in admin dashboard

--- src/pages/Dashboard/AdminDashboard/AllMedicines/AllMedicines.jsx
+++ src/pages/Dashboard/AdminDashboard/AllMedicines/AllMedicines.jsx
@@ -1,105 +1,87 @@
+/* eslint-disable no-unsafe-optional-chaining */
 import { RiDeleteBinLine } from "react-icons/ri";
 import { TiEdit } from "react-icons/ti";
-import { NavLink } from "react-router-dom";
+import useMedicines from "../../../../hooks/useMedicines";
+import { Menu, MenuButton, MenuItem } from "@szhsin/react-menu";
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
+import { useState } from "react";
 
 const AllMedicines = () => {
+  const [medicines] = useMedicines();
+  const [showDropdown2, setShowDropdown2] = useState(false);
+
+  const toggleDropdown2 = () => {
+    setShowDropdown2(!showDropdown2);
+  };
+
   return (
     <div className="mx-1 md:mx-5">
-      <h3 className="text-center text-3xl my-7 font-semibold">
+      <h3 className="text-center text-xl lg:text-3xl my-7 font-semibold tracking-wide">
         All Available Medicines
       </h3>
 
-      <div className="overflow-x-auto mb-20  px-5">
-        <table className="table bg-gray-3">
+      <div className="overflow-x-auto mb-20 px-5">
+        <table className="table rounded bg-lite">
           {/* head */}
-          <thead className="rounded-lg bg-my-accent text-lg">
-            <tr className="bg-my-secondary rounded-lg">
+          <thead className="bg-my-primary text-white font-normal text-base">
+            <tr className="">
               <th>#</th>
               <th>Photo</th>
               <th>Name</th>
-              <th>Generic</th>
-              <th>Supplier</th>
-
+              <th>Pharmacist Info</th>
               <th>Price</th>
               <th>Av. Quantity</th>
+              <th>Status</th>
               <th>Action</th>
             </tr>
           </thead>
-
           <tbody>
-            {/* row 1 */}
-            <tr className="">
-              <td>1</td>
-              <td>
-                <img
-                  className="w-10 h-10 rounded-full"
-                  src="https://t4.ftcdn.net/jpg/02/81/42/77/360_F_281427785_gfahY8bX4VYCGo6jlfO8St38wS9cJQop.jpg"
-                  alt=""
-                />
-              </td>
-              <td>NAPA EXTEND 665 TAB</td>
-              <td>PARACETAMOL BP</td>
-              <td>BEXIMCO PHARMACEUTICALS</td>
-              <td>6.50Tk</td>
+            {/* rows */}
+            {
+              medicines?.map((medicine, idx) => (
+                <tr key={medicine?._id} className="border-b border-slate-3">
+                  <td>{idx + 1}</td>
+                  <td>
+                    <img
+                      className="mask rounded w-14 h-14"
+                      src={medicine?.image}
+                      alt="medicine"
+                    />
+                  </td>
+                  <td className="font-medium">{medicine?.medicine_name}</td>
+                  <td className="flex flex-col">
+                    <span>{medicine?.pharmacist_name}</span>
+                    <span>{medicine?.pharmacist_email}</span>
+                  </td>
+                  <td>৳ {medicine?.price}</td>
+                  <td className="font-medium"><span className="text-my-pink">{medicine?.available_quantity - medicine?.sellQuantity}</span> / {medicine?.available_quantity}</td>
 
-              <td>800</td>
-              <td className="flex items-center gap-2">
-                <NavLink>
-                  <TiEdit className="text-2xl p-1 text-white bg-my-primary" />
-                </NavLink>
-                <NavLink>
-                  <RiDeleteBinLine className="text-2xl bg-red-500 text-white p-1" />
-                </NavLink>
-              </td>
-            </tr>
-            <tr className="">
-              <td>1</td>
-              <td>
-                <img
-                  className="w-10 h-10 rounded-full"
-                  src="https://images.newscientist.com/wp-content/uploads/2019/11/27120939/d7mw20.jpg?width=800"
-                  alt=""
-                />
-              </td>
-              <td>NAPA EXTEND 665 TAB</td>
-              <td>PARACETAMOL BP</td>
-              <td>BEXIMCO PHARMACEUTICALS</td>
-              <td>6.50Tk</td>
-
-              <td>800</td>
-              <td className="flex items-center gap-2">
-                <NavLink>
-                  <TiEdit className="text-2xl p-1 text-white bg-my-primary" />
-                </NavLink>
-                <NavLink>
-                  <RiDeleteBinLine className="text-2xl bg-red-500 text-white p-1" />
-                </NavLink>
-              </td>
-            </tr>
-            <tr className="">
-              <td>1</td>
-              <td>
-                <img
-                  className="w-10 h-10 rounded-full"
-                  src="https://t4.ftcdn.net/jpg/02/81/42/77/360_F_281427785_gfahY8bX4VYCGo6jlfO8St38wS9cJQop.jpg"
-                  alt=""
-                />
-              </td>
-              <td>NAPA EXTEND 665 TAB</td>
-              <td>PARACETAMOL BP</td>
-              <td>BEXIMCO PHARMACEUTICALS</td>
-              <td>6.50Tk</td>
-
-              <td>800</td>
-              <td className="flex items-center gap-2">
-                <NavLink>
-                  <TiEdit className="text-2xl p-1 text-white bg-my-primary" />
-                </NavLink>
-                <NavLink>
-                  <RiDeleteBinLine className="text-2xl bg-red-500 text-white p-1" />
-                </NavLink>
-              </td>
-            </tr>
+                  <td className={`${medicine.status === "approved" && "text-my-accent"} ${medicine.status === "denied" && "text-red-500"} ${medicine.status === "pending" && "text-yellow-500"} capitalize font-medium`}>{medicine?.status}</td>
+                  <td>
+                    <Menu
+                      menuButton={
+                        // eslint-disable-next-line react/jsx-wrap-multilines
+                        <MenuButton>
+                          <button onClick={toggleDropdown2} type="button" className="btn-sm inline-flex items-center bg-my-accent text-white w-full">
+                            <span>Pending</span>
+                            <MdKeyboardArrowDown className={`${showDropdown2 ? "hidden" : "block"} dashboard-icon`} />
+                            <MdKeyboardArrowUp className={`${showDropdown2 ? "block" : "hidden"} dashboard-icon`} />
+                          </button>
+                        </MenuButton>
+                      }
+                      transition
+                    >
+                      <MenuItem className="font-semibold text-gray-6">
+                        Approve
+                      </MenuItem>
+                      <MenuItem className="font-semibold text-gray-6">
+                        Deny
+                      </MenuItem>
+                    </Menu>
+                  </td>
+                </tr>
+              ))
+            }
           </tbody>
         </table>
       </div>
