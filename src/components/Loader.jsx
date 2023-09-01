25f6a58a0a852d2de4ed78d2f92e668c11790ce0
unknown <sihabuddinmolla11@gmail.com>
2023-09-01 18:24:06 +0600

    fixed website

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import { RiseLoader } from "react-spinners";
+
+const Loader = () => {
+  return (
+    <div className=" w-full flex justify-center h-full items-center mt-40">
+      <RiseLoader color="#006F70" />
+    </div>
+  );
+};
+
+export default Loader;
