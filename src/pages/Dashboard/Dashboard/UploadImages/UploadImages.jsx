2337534b2282a3bfecfa4075573a402847ef7453
tapfer-abdullah <tapfer.abdullah@gmail.com>
2023-09-05 12:44:01 +0600

    upload image api created

---
+++ src/pages/Dashboard/Dashboard/UploadImages/UploadImages.jsx
@@ -0,0 +1,13 @@
+const UploadImages = () => {
+  return (
+    <div className="px-5">
+      <h3 className="text-center text-3xl my-7 font-semibold">All Images</h3>
+
+      <div className="bg-slate-3">
+        <img src="https://i.ibb.co/dc4MPPJ/3646374.jpg" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default UploadImages;
