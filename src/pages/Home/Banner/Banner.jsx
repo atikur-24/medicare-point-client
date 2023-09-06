03d065906494683d84b4c511bfa8bd2279293a19
Abdullah149081 <masudmd425@gmail.com>
2023-09-06 13:05:46 +0600

    banner change

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -4,11 +4,11 @@ import "swiper/css/pagination";
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
-import image2 from "../../../assets/images/banner/image2.webp";
+import image2 from "../../../assets/images/banner/image2.png";
 // import image3 from "../../../assets/images/banner/image3.webp";
 import image4 from "../../../assets/images/banner/image4.webp";
-import image5 from "../../../assets/images/banner/image5.webp";
-import image6 from "../../../assets/images/banner/image6.webp";
+// import image5 from "../../../assets/images/banner/image5.webp";
+// import image6 from "../../../assets/images/banner/image6.webp";
 import image7 from "../../../assets/images/banner/image7.webp";
 import Search from "../../Shared/Navbar/Search/Search";
 
@@ -41,12 +41,12 @@ const Banner = () => {
         <SwiperSlide>
           <img className="h-1/3 w-full" src={image4} alt="" />
         </SwiperSlide>
-        <SwiperSlide>
+        {/* <SwiperSlide>
           <img className="h-1/3 w-full" src={image5} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
+        </SwiperSlide> */}
+        {/* <SwiperSlide>
           <img className="h-1/3 w-full" src={image6} alt="" />
-        </SwiperSlide>
+        </SwiperSlide> */}
         <SwiperSlide>
           <img className="h-1/3 w-full" src={image7} alt="" />
         </SwiperSlide>
