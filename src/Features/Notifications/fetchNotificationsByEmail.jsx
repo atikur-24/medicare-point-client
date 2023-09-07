6f8b51e4d9518b7fb1024ad6b9d1ce7041ebbfa2
tapfer-abdullah <tapfer.abdullah@gmail.com>
2023-09-08 00:38:06 +0600

    notification done

---
+++ src/Features/Notifications/fetchNotificationsByEmail.jsx
@@ -0,0 +1,34 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchNotificationsByEmail = createAsyncThunk("notificationsByEmail/fetchNotificationsByEmail", async (email) => {
+  const res = await axios.get(`http://localhost:5000/notifications?email=${email}`);
+  //   console.log(res.data);
+  return res.data;
+});
+
+const notificationsByEmailSlice = createSlice({
+  name: "notificationsByEmail",
+  initialState: {
+    isLoading: false,
+    error: null,
+    allNotifications: [],
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchNotificationsByEmail.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchNotificationsByEmail.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.allNotifications = action.payload;
+      state.error = null;
+    });
+    builder.addCase(fetchNotificationsByEmail.rejected, (state, action) => {
+      state.isLoading = false;
+      state.allNotifications = [];
+      state.error = action.error.message;
+    });
+  },
+});
+
+export default notificationsByEmailSlice.reducer;
