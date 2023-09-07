eb011e8445a9f62ae9d55b5404a2cc818b683394
safayatalif <safayatalif2021@gmail.com>
2023-09-07 14:50:25 +0600

    Lab Partner , Lab Work , Lab Book , Lab FAQs section create complate

---
+++ src/pages/Shared/LabTest/LabPartner.jsx
@@ -0,0 +1,58 @@
+/* eslint-disable import/no-unresolved */
+
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import { FreeMode, Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import image1 from "../../../assets/Lab/HealthLab.png";
+import image3 from "../../../assets/Lab/LifeScan.png";
+import image2 from "../../../assets/Lab/TestPro.png";
+
+const LabPartner = () => {
+  return (
+    <div className="my-16 grid md:grid-cols-3 grid-cols-1 md:gap-8">
+      <div className="rounded-lg bg-my-accent w-full h-36 md:h-full flex items-center justify-center text-3xl font-semibold mb-6">Lab Partner</div>
+      <div className="col-span-2">
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          freeMode
+          pagination={{
+            clickable: true,
+          }}
+          modules={[FreeMode, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <div className="py-4 object-cover border border-my-accent rounded-lg text-center space-y-4 h-44">
+              <img src={image1} alt="" className="w-20 mx-auto" />
+              <h3 className="text-lg font-medium">HealthLab</h3>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="py-4 object-cover border border-my-accent rounded-lg text-center space-y-4 h-44">
+              <img src={image2} alt="" className="w-20 mx-auto" />
+              <h3 className="text-lg font-medium">LifeScan Laboratories</h3>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="py-4 object-cover border border-my-accent rounded-lg text-center space-y-4 h-44">
+              <img src={image3} alt="" className="w-20 mx-auto" />
+              <h3 className="text-lg font-medium">ProHealth Labs</h3>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="py-4 object-cover border border-my-accent rounded-lg text-center space-y-4 h-44">
+              <img src={image1} alt="" className="w-20 mx-auto" />
+              <h3 className="text-lg font-medium">DiagnoTech Solutions</h3>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
+export default LabPartner;
--- src/pages/Shared/LabTest/LabTest.jsx
+++ src/pages/Shared/LabTest/LabTest.jsx
@@ -1,6 +1,10 @@
 import CheckCard from "./CheckCard/CheckCard";
 import LabBanner from "./LabBanner/LabBanner";
+import LabBook from "./LabBook";
+import LabFaqs from "./LabFaqs";
+import LabPartner from "./LabPartner";
 import LabSearch from "./LabSearch/LabSearch";
+import LabWork from "./LabWork";
 import PopularLab from "./PopularLab";
 import TopTest from "./TopTest";
 
@@ -13,6 +17,10 @@ const LabTest = () => {
         <LabSearch />
         <PopularLab />
         <TopTest />
+        <LabWork />
+        <LabPartner />
+        <LabBook />
+        <LabFaqs />
       </div>
     </div>
   );
