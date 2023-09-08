94e10620455f9e46c8fea3254eff5f7b9b3e0c71
tapfer-abdullah <tapfer.abdullah@gmail.com>
2023-09-08 15:24:05 +0600

    dynamic dashboard added

--- src/pages/Dashboard/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard/Dashboard.jsx
@@ -1,28 +1,34 @@
-import { useEffect, useState } from "react";
-import useAuth from "../../../hooks/useAuth";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../../contexts/AuthProvider";
 import AdminHome from "../AdminDashboard/AdminHome/AdminHome";
-
 import PharmacistsHome from "../PharmacistDashboard/PharmacistsHome/PharmacistsHome";
+import UserProfile from "../UserDashboard/UserProfile/UserProfile";
 
 const Dashboard = () => {
-  const { role } = useAuth();
+  const { role } = useContext(AuthContext);
   const [isUser, setUser] = useState(false);
   const [isPharmacist, setPharmacist] = useState(false);
   const [isAdmin, setAdmin] = useState(false);
 
   useEffect(() => {
-    if (role === "User") {
+    if (role === "user") {
       setUser(true);
     } else if (role === "Pharmacist") {
       setPharmacist(true);
-    } else if (role === "Admin") {
+    } else if (role === "admin") {
       setAdmin(true);
     }
-  }, []);
+  }, [role]);
+
+  // if (!role) {
+  //   return <Loader />;
+  // }
+
   return (
     <div>
       {isAdmin && <AdminHome />}
       {isPharmacist && <PharmacistsHome />}
+      {isUser && <UserProfile />}
     </div>
   );
 };
