9f59402a6f993eaed305363f520e3f702c2ab90d
tapfer-abdullah <tapfer.abdullah@gmail.com>
2023-09-08 15:24:05 +0600

    dynamic dashboard added

--- src/pages/Dashboard/UserDashboard/RewardPoints/RewardPoints.jsx
+++ src/pages/Dashboard/UserDashboard/RewardPoints/RewardPoints.jsx
@@ -1,26 +1,11 @@
 import ProgressBar from "@ramonak/react-progress-bar";
-import { useContext, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
-import { fetchUserByEmail } from "../../../../Features/AllUsers/userByEmail";
 import { AuthContext } from "../../../../contexts/AuthProvider";
 import "./RewardPoints.css";
 
 const RewardPoints = () => {
-  const { user } = useContext(AuthContext);
-  const dispatch = useDispatch();
-
-  const { userByEmail } = useSelector((state) => state);
-
-  useEffect(() => {
-    if (user?.email) {
-      dispatch(fetchUserByEmail(user?.email));
-    }
-  }, [user?.email, dispatch]);
-
-  if (userByEmail.isLoading) {
-    return <p className="text-center mt-10">Loading.......</p>;
-  }
+  const { userInfo } = useContext(AuthContext);
 
   return (
     <div className="px-5 mt-10">
@@ -29,13 +14,13 @@ const RewardPoints = () => {
       <div className="reward-card p-5 bg-opacity-10 rounded-md space-y-5">
         <div className="grid grid-cols-2">
           <div className="text-white">
-            <h3 className="text-2xl font-semibold">{userByEmail?.user?.name}</h3>
-            <p className="text-lg font-medium">{userByEmail?.user?.email}</p>
+            <h3 className="text-2xl font-semibold">{userInfo?.name}</h3>
+            <p className="text-lg font-medium">{userInfo?.email}</p>
           </div>
 
           <div className="text-white text-end">
             <h3 className="font-medium text-lg">MCP Points</h3>
-            <p className="text-2xl font-semibold">{userByEmail?.user?.rewardPoints}</p>
+            <p className="text-2xl font-semibold">{userInfo?.rewardPoints}</p>
           </div>
         </div>
 
@@ -43,16 +28,16 @@ const RewardPoints = () => {
           <div className="flex flex-col justify-center items-center text-white font-semibold">
             <img className="w-10 h-10" src="https://i.ibb.co/80RgtPb/star.png" alt="" />
 
-            {userByEmail?.user?.rewardPoints < 100 && <span>Bronze</span>}
-            {userByEmail?.user?.rewardPoints >= 100 && userByEmail?.user?.rewardPoints < 250 && <span>Silver</span>}
-            {userByEmail?.user?.rewardPoints >= 250 && userByEmail?.user?.rewardPoints < 400 && <span>Gold</span>}
-            {userByEmail?.user?.rewardPoints >= 100 && userByEmail?.user?.rewardPoints < 250 && <span>Silver</span>}
-            {userByEmail?.user?.rewardPoints >= 400 && userByEmail?.user?.rewardPoints < 750 && <span>Silver</span>}
-            {userByEmail?.user?.rewardPoints >= 750 && <span>Silver</span>}
+            {userInfo?.rewardPoints < 100 && <span>Bronze</span>}
+            {userInfo?.rewardPoints >= 100 && userInfo?.rewardPoints < 250 && <span>Silver</span>}
+            {userInfo?.rewardPoints >= 250 && userInfo?.rewardPoints < 400 && <span>Gold</span>}
+            {userInfo?.rewardPoints >= 100 && userInfo?.rewardPoints < 250 && <span>Silver</span>}
+            {userInfo?.rewardPoints >= 400 && userInfo?.rewardPoints < 750 && <span>Silver</span>}
+            {userInfo?.rewardPoints >= 750 && <span>Silver</span>}
           </div>
 
           <div className="w-full space-y-1">
-            <ProgressBar className="w-full" height="10px" completed={userByEmail?.user?.rewardPoints || 0} maxCompleted={1000} customLabel=" " baseBgColor="#10B6A8" bgColor="#ffc107" />
+            <ProgressBar className="w-full" height="10px" completed={userInfo?.rewardPoints || 0} maxCompleted={1000} customLabel=" " baseBgColor="#10B6A8" bgColor="#ffc107" />
             <div className="flex justify-between font-semibold text-slate-3">
               <p>Silver</p>
               <p>Gold</p>
