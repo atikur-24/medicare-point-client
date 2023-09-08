67fea25bd6f4e79a2184dc4c76da339c058ab922
unknown <sihabuddinmolla11@gmail.com>
2023-09-08 16:38:56 +0600

    fixed responsive navbar problem

--- src/pages/Shared/Navbar/ResponsiveNavbar/ResponsiveNavbar.jsx
+++ src/pages/Shared/Navbar/ResponsiveNavbar/ResponsiveNavbar.jsx
@@ -37,7 +37,7 @@ const ResponsiveNavbar = ({ menuItems }) => {
   useEffect(() => {
     const handelOutsiteClose = (e) => {
       if (!menuRef?.current?.contains(e?.target)) {
-        setIsOpen(false);
+        setIsOpen("-ml-96");
       }
     };
     document.addEventListener("mousedown", handelOutsiteClose);
@@ -103,7 +103,7 @@ const ResponsiveNavbar = ({ menuItems }) => {
       <hr className="w-full  shadow-md border border-my-primary" />
 
       {/* {isOpen && ( */}
-      <div className={`z-50 shadow-2xl bg-white w-[50%] h-full border-2 rounded-lg border-gray-3 absolute top-0 left-0 ${isOpen} transition-all duration-500`} ref={menuRef}>
+      <div className={`z-50 shadow-2xl bg-white w-[70%] h-full border-2 rounded-lg border-gray-3 absolute top-0 left-0 ${isOpen} transition-all duration-500`} ref={menuRef}>
         <div className=" pt-[37px] ">
           <div className=" px-4 flex justify-between items-center mb-5">
             <div>
