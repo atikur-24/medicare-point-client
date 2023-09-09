0ff96d3657abc2c535027a08f6db3d95a5406243
unknown <sihabuddinmolla11@gmail.com>
2023-09-09 23:20:11 +0600

    create loader

--- src/pages/Home/Painrelief/PainProducts.jsx
+++ src/pages/Home/Painrelief/PainProducts.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable import/order */
 /* eslint-disable import/no-unresolved */
-import axios from "axios";
-import { useEffect, useState } from "react";
+
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "swiper/css";
 import "swiper/css/pagination";
@@ -10,6 +10,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { fetchMedicines } from "../../../Features/Medicines/AllMedicines/allMedicines";
 import bannerImage from "../../../assets/images/services/01.jpg";
 import bannerBackground from "../../../assets/images/services/features.jpg";
+import Loader from "../../../components/Loader";
 import SectionTitle from "../../../components/SectionTitle";
 import MediCard from "../../Shared/Card/MediCard";
 import CommonBanner from "../../Shared/CommonBanner/CommonBanner";
@@ -23,70 +24,70 @@ const MediCards = () => {
 
   const PainRefilMedicins = allData.filter((item) => item?.category?.value === "Pain-Relief");
 
-  if (isloading) {
-    return <div className="my-container text-center">Loging .............</div>;
-  }
-
   return (
     <>
       <div className="my-container my-10">
         <SectionTitle title="Pain Relief" content="Pain Relief medicines is like a helper for when something hurts. It's a way to make the hurt feel better." />
 
-        <div>
-          <div className="hidden lg:block">
-            <Swiper
-              slidesPerView={4}
-              spaceBetween={50}
-              pagination={{
-                clickable: true,
-              }}
-              modules={[Pagination]}
-              className="mySwiper"
-            >
-              {PainRefilMedicins?.map((medicine) => (
-                <SwiperSlide key={medicine?._id}>
-                  <MediCard medicine={medicine} />
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          </div>
+        {isloading ? (
+          <Loader spinner />
+        ) : (
+          <div>
+            <div className="hidden lg:block">
+              <Swiper
+                slidesPerView={4}
+                spaceBetween={50}
+                pagination={{
+                  clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+              >
+                {PainRefilMedicins?.map((medicine) => (
+                  <SwiperSlide key={medicine?._id}>
+                    <MediCard medicine={medicine} />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
 
-          <div className="hidden md:block lg:hidden">
-            <Swiper
-              slidesPerView={2}
-              spaceBetween={50}
-              pagination={{
-                clickable: true,
-              }}
-              modules={[Pagination]}
-              className="mySwiper"
-            >
-              {PainRefilMedicins.map((medicine) => (
-                <SwiperSlide key={medicine?._id}>
-                  <MediCard medicine={medicine} />
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          </div>
+            <div className="hidden md:block lg:hidden">
+              <Swiper
+                slidesPerView={2}
+                spaceBetween={50}
+                pagination={{
+                  clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+              >
+                {PainRefilMedicins.map((medicine) => (
+                  <SwiperSlide key={medicine?._id}>
+                    <MediCard medicine={medicine} />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
 
-          <div className="md:hidden">
-            <Swiper
-              slidesPerView={1}
-              spaceBetween={50}
-              pagination={{
-                clickable: true,
-              }}
-              modules={[Pagination]}
-              className="mySwiper"
-            >
-              {PainRefilMedicins.map((medicine) => (
-                <SwiperSlide key={medicine?._id}>
-                  <MediCard medicine={medicine} />
-                </SwiperSlide>
-              ))}
-            </Swiper>
+            <div className="md:hidden">
+              <Swiper
+                slidesPerView={1}
+                spaceBetween={50}
+                pagination={{
+                  clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+              >
+                {PainRefilMedicins.map((medicine) => (
+                  <SwiperSlide key={medicine?._id}>
+                    <MediCard medicine={medicine} />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
           </div>
-        </div>
+        )}
       </div>
       <CommonBanner
         bgImage={bannerBackground}
