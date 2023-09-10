bae605cdd2553527c7153241af9c4296576adf7b
unknown <sihabuddinmolla11@gmail.com>
2023-09-10 23:49:08 +0600

    pharmaciest application detail modify

--- src/pages/Dashboard/AdminDashboard/AllMedicines/AllMedicines.jsx
+++ src/pages/Dashboard/AdminDashboard/AllMedicines/AllMedicines.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-useless-fragment */
 /* eslint-disable no-unsafe-optional-chaining */
 import { Menu, MenuButton, MenuItem } from "@szhsin/react-menu";
 import axios from "axios";
@@ -18,35 +19,58 @@ const AllMedicines = () => {
   const handelChangeStatus = (id, newStatus) => {
     const statusApproved = { status: newStatus };
     axios.patch(`http://localhost:5000/medicine-status/${id}`, statusApproved).then((res) => {
-      console.log(res);
+      console.log(res.data);
       dispatch(fetchAllMedicines());
     });
   };
 
+  const approvedMedicines = medicines.filter((medicine) => medicine?.status === "approved");
+  const pendingMedicines = medicines.filter((medicine) => medicine?.status === "pending");
+  const deniedMedicines = medicines.filter((medicine) => medicine?.status === "denied");
+
   return (
     <div className="mx-1 md:mx-5">
-      <h3 className="text-center text-xl lg:text-3xl my-7 font-semibold tracking-wide">All Available Medicines</h3>
+      {/* <h3 className="text-center text-xl lg:text-3xl my-7 font-semibold tracking-wide">All Available Medicines</h3> */}
+
+      <div className="flex px-6 mb-8">
+        <div className="stats shadow">
+          <div className="stat place-items-center space-y-2">
+            <div className="stat-title text-title-color font-nunito font-bold uppercase ">Approved Medicines</div>
+            <div className="stat-value text-my-primary">{approvedMedicines?.length}</div>
+          </div>
+
+          <div className="stat place-items-center space-y-2">
+            <div className="stat-title text-title-color font-nunito font-bold uppercase ">Pending Medicines</div>
+            <div className="stat-value text-yellow-500">{pendingMedicines?.length}</div>
+          </div>
+
+          <div className="stat place-items-center space-y-2">
+            <div className="stat-title text-title-color font-nunito font-bold uppercase ">Denied Medicines</div>
+            <div className="stat-value text-red-500">{deniedMedicines?.length}</div>
+          </div>
+        </div>
+      </div>
 
       <div className=" mb-20 px-5">
-        {isLoading ? (
-          <Loader spinner />
-        ) : (
-          <table className="overflow-x-auto table rounded bg-lite">
-            {/* head */}
-            <thead className="bg-my-primary text-white font-normal text-base">
-              <tr className="">
-                <th>#</th>
-                <th>Photo</th>
-                <th>Name</th>
-                <th>Pharmacist Info</th>
-                <th>Price</th>
-                <th>Av. Quantity</th>
-                <th>Status</th>
-                <th>Action</th>
-              </tr>
-            </thead>
+        <table className="overflow-x-auto table rounded bg-lite">
+          {/* head */}
+          <thead className="bg-my-primary text-white font-normal text-base">
+            <tr className="">
+              <th>#</th>
+              <th>Photo</th>
+              <th>Name</th>
+              <th>Pharmacist Info</th>
+              <th>Price</th>
+              <th>Av. Quantity</th>
+              <th>Status</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+
+          {/* rows */}
+
+          {!isLoading && (
             <tbody>
-              {/* rows */}
               {medicines?.map((medicine, idx) => (
                 <tr key={medicine?._id} className="border-b border-slate-3">
                   <td>{idx + 1}</td>
@@ -95,7 +119,12 @@ const AllMedicines = () => {
                 </tr>
               ))}
             </tbody>
-          </table>
+          )}
+        </table>
+        {isLoading && (
+          <div className="mt-44">
+            <Loader spinner />
+          </div>
         )}
       </div>
     </div>
