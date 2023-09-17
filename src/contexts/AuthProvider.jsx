1e6f06d01f5e812b8295a29defce176b61a0b6a6
tapfer-abdullah <tapfer.abdullah@gmail.com>
2023-09-17 21:48:47 +0600

    admin dashboard home editing

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -21,7 +21,6 @@ export const AuthContext = createContext(null);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 
-
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [userInfo, setUserInfo] = useState({});
@@ -64,7 +63,6 @@ const AuthProvider = ({ children }) => {
       photoURL: photo,
     });
   };
-
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
