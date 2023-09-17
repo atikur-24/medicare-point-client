f29efe4e1760ff5bc36084512909bd48a0ba06b0
unknown <sihabuddinmolla11@gmail.com>
2023-09-17 12:42:17 +0600

    inplement redux in healt tips page and blog page

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -1,26 +1,34 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAllData } from "../../Features/AllMedicines/allData";
 import blogImage from "../../assets/Blog/blog.webp";
+import Loader from "../../components/Loader";
 import Blog from "./Blog";
 import "./Blog.css";
 
 const Blogs = () => {
-  const [blogs, setBlogs] = useState([]);
-
+  const api = "blogs";
+  const { isLoading, allData: blogs } = useSelector((state) => state.allData);
+  const dispatch = useDispatch();
   useEffect(() => {
-    axios.get("http://localhost:5000/blogs").then((res) => setBlogs(res.data));
-  }, []);
+    dispatch(fetchAllData(api));
+  }, [dispatch, api]);
+
   return (
     <div className="bg-white">
       <div>
         <img className="w-full" src={blogImage} alt="" />
       </div>
       <section className="my-container">
-        <div className="">
-          {blogs.map((blog) => (
-            <Blog range={300} blog={blog} key={blog._id} />
-          ))}
-        </div>
+        {isLoading ? (
+          <Loader spinner />
+        ) : (
+          <div className="">
+            {blogs?.map((blog) => (
+              <Blog range={300} blog={blog} key={blog._id} />
+            ))}
+          </div>
+        )}
       </section>
     </div>
   );
