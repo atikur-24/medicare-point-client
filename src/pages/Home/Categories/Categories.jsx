31bd97e0eae3d79a6053be7f339967cb5f64861b
Atikur <atikurrahman7223@gmail.com>
2023-09-20 18:11:50 +0600

    show top rated medicine and higest selling medicines

--- src/pages/Home/Categories/Categories.jsx
+++ src/pages/Home/Categories/Categories.jsx
@@ -60,39 +60,9 @@ const Categories = () => {
             </SwiperSlide>
           ))}
         </Swiper>
-
-        {/* <Swiper
-          onSwiper={setSwiperRef}
-          slidesPerView={6}
-          centeredSlides
-          spaceBetween={30}
-          pagination={{
-            type: "fraction",
-          }}
-          navigation
-          modules={[Pagination, Navigation]}
-          className="mySwiper"
-        >
-          {categories.map((category, index) => (
-            <SwiperSlide key={index}>
-              <Category category={category} />
-            </SwiperSlide>
-          ))}
-        </Swiper> */}
       </div>
     </div>
   );
-
-  // return (
-  //   <div className="my-container">
-  //     <SectionTitle title="Shop by Category" content="Discover essential healthcare solutions through our diverse medicine categories. explore now and prioritize your health journey with us." />
-  //     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4  xl:grid-cols-8 gap-4">
-  //       {categories.map((category, index) => (
-  //         <Category key={index} category={category} />
-  //       ))}
-  //     </div>
-  //   </div>
-  // );
 };
 
 export default Categories;
