48b8fa7c0af554dfa36780381537bf4af27cffd1
unknown <sihabuddinmolla11@gmail.com>
2023-09-22 15:02:56 +0600

    fixed admin dashboard design problem

--- src/pages/Dashboard/AdminDashboard/MedicineReturnByAdmin/MedicineReturnByAdmin.jsx
+++ src/pages/Dashboard/AdminDashboard/MedicineReturnByAdmin/MedicineReturnByAdmin.jsx
@@ -1,8 +1,15 @@
 const MedicineReturnByAdmin = () => {
   return (
-    <section className="bg-lite py-6 lg:py-10 px-4 lg:px-8">
+    <section className=" py-6 lg:py-10 ">
       <div className=" space-y-2 mb-5">
-        <h3 className="text-xl lg:text-3xl tracking-wider font-medium">Return Orders</h3>
+        <div className="flex  mb-8">
+          <div className="stats shadow">
+            <div className="stat place-items-center space-y-2">
+              <div className="stat-title text-title-color font-nunito font-bold uppercase ">Return Order</div>
+              <div className="stat-value text-my-primary">0</div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="flex border-b flex-col md:flex-row bg-white rounded-md gap-5 border-gray-3 md:items-center md:justify-between p-5">
         <h2 className="text-base lg:text-lg">No Delivered Product</h2>
