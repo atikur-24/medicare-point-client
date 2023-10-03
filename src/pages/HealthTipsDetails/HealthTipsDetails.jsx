0de0410213b54c7bd70d1912f5fac6fe45f2ede3
Abdullah149081 <masudmd425@gmail.com>
2023-10-03 17:40:14 +0600

    health details page clean fx

--- src/pages/HealthTipsDetails/HealthTipsDetails.jsx
+++ src/pages/HealthTipsDetails/HealthTipsDetails.jsx
@@ -85,11 +85,11 @@ const HealthTipsDetails = () => {
           <div className="grid grid-cols-1 lg:grid-cols-1 gap-4 items-center mt-5">
             <div className=" h-full p-1">
               <h3 className="text-2xl font-semibold my-2">How To Prevent?</h3>
-              <p>{HtmlParser(diseaseDetails.prevention)}</p>
+              <div>{HtmlParser(diseaseDetails.prevention)}</div>
             </div>
             <div className="h-full p-1">
               <h3 className="text-2xl font-semibold my-2">What is the cure?</h3>
-              <p>{HtmlParser(diseaseDetails.cure)}</p>
+              <div>{HtmlParser(diseaseDetails.cure)}</div>
             </div>
           </div>
         </div>
